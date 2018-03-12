e4d77f85297830a1d27a7a221f7df492c19baeb7
Mark Henderson <mhenderson@stackoverflow.com>
2018-03-12 11:47:48 -0400

    No linux git detection yet

--- cmd/go-blackbox/vcsproviders/git/git_unix.go
+++ cmd/go-blackbox/vcsproviders/git/git_unix.go
@@ -2,12 +2,8 @@
 
 package git
 
-import (
-	"fmt"
-)
-
 // Set REPOBASE to the top of the repository
 func (vcs *vcs) GetRepoBase() string {
 	//gitOut, err := exec.Command("which >/dev/null 2>/dev/null git && git rev-parse --show-toplevel >/dev/null 2>&1").CombinedOutput()
-	return "", fmt.Errorf("not implemented on Linux yet")
+	return "" //fmt.Errorf("not implemented on Linux yet")
 }
