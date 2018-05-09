3f5526d3d90963c85e70efd5241825f38ae93ee1
Mark Henderson <mhenderson@stackoverflow.com>
2018-05-09 15:38:51 -0400

    Few more minor changes to initialize

--- cmd/blackbox/initialize.go
+++ cmd/blackbox/initialize.go
@@ -9,25 +9,31 @@ import (
 )
 
 func initialize() error {
-	cwd, _ := os.Getwd()
 	fmt.Print("Enable blackbox for this repo? (yes/no) ")
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
 
+	//Why do we do it this way? Only to maintain compatibility with the existing
+	//blackbox functionality, as it works this way
 	if text != "yes" {
 		if text == "no" || text == "n" || text == "" {
 			return nil
 		}
 	}
 
+	//Try to initialize
+	cwd, _ := os.Getwd()
 	err := blackbox.Initialize(cwd)
 	if err != nil {
 		fmt.Println("Unable to initialize blackbox:", err)
+		return err
 	}
 
+	//If we initialized succesfully then give the user some feedback
 	setup("")
 	fmt.Println("VCS_TYPE:", blackbox.VCSType)
 	fmt.Println()
 	fmt.Println("You need to manually check in your new blackbox files")
+
 	return nil
 }
